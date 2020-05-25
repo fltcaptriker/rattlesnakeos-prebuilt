20eab438b37a3f181c81da66c91d410503ebc613
fltcaptriker <fltcaptriker@gmail.com>
2020-05-25 17:24:27 -0400

    Create Android.mk

---
+++ EuiccGoogle/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := EuiccGoogle
+LOCAL_MODULE_OWNER := google
+LOCAL_SRC_FILES := EuiccGoogle.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+
+include $(BUILD_PREBUILT)
