4330be962be9e8678c4ffea52365f4f30a00dcf8
fltcaptriker <fltcaptriker@gmail.com>
2020-05-26 09:53:57 -0400

    Update Android.mk

--- EuiccGoogle/Android.mk
+++ EuiccGoogle/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := EuiccGoogle
 LOCAL_SRC_FILES := EuiccGoogle.apk
-
+LOCAL_PACKAGE_NAME := com.google.android.euicc
 LOCAL_PRIVILEGED_MODULE := true
 
 include $(BUILD_PREBUILT)
