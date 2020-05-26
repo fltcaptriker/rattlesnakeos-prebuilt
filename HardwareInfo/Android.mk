b7c11ae3febb88763b0a2b8512b2251568567a46
fltcaptriker <fltcaptriker@gmail.com>
2020-05-26 17:41:57 -0400

    Update Android.mk

--- HardwareInfo/Android.mk
+++ HardwareInfo/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := HardwareInfo
-LOCAL_SRC_FILES := HardwarInfo.apk
+LOCAL_SRC_FILES := HardwareInfo.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PACKAGE_NAME := com.google.android.hardwareinfo
 LOCAL_PRIVILEGED_MODULE := true
