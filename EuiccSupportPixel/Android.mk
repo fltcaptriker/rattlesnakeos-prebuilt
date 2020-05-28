1fc59c700de80ff161ed5ddd0baf653f9aa4a7b9
fltcaptriker <fltcaptriker@gmail.com>
2020-05-27 23:56:40 -0400

    Update Android.mk

--- EuiccSupportPixel/Android.mk
+++ EuiccSupportPixel/Android.mk
@@ -2,11 +2,14 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := EuiccSupportPixel
+LOCAL_MODULE_OWNER := google
 LOCAL_SRC_FILES := EuiccSupportPixel.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PACKAGE_NAME := com.google.euiccpixel
+LOCAL_MODULE_TAGS := optional
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_SUFFIX := .apk
 TARGET_OUT_DATA_APPS_PRIVILEGED := $(TARGET_OUT_DATA)/product/priv-app
 
 include $(BUILD_PREBUILT)
